a415b68361ac215dabda4eea82dd8dbf24f21ea0
Aron Rop <aaronrop40@gmail.com>
2026-07-11 21:13:13 +0300

    created and admin dashboard template

---
+++ app/forms/product_forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField, DecimalField, IntegerField, BooleanField, SelectField, SubmitField)
+from wtforms.validators import DataRequired, NumberRange
+
+
+class ProductForm(FlaskForm):
+    name = StringField("Product Name", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    price = DecimalField("Price (KSh)", validators=[DataRequired(), NumberRange(min=0)], places=2)
+    stock = IntegerField("Stock Quantity", validators=[DataRequired(), NumberRange(min=0)])
+    sku = StringField("SKU")
+    category = SelectField("Category", coerce=int, validators=[DataRequired()])
+    featured = BooleanField("Featured Product")
+    submit = SubmitField("Save Product")
--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, abort
+from flask_login import login_required, current_user
 
 admin_bp = Blueprint("admin", __name__)
 
+
 @admin_bp.route("/")
+@login_required
 def dashboard():
-    return "<h1>AgroMart Admin Dashboard</h1>"
+
+    if current_user.role != "admin":
+        abort(403)
+
+    return render_template("admin/dashboard.html")
