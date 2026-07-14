bdafd4411fd292516bd5b5f7e6c41922d1232c5a
Aron Rop <aaronrop40@gmail.com>
2026-07-14 20:32:01 +0300

    modified my admin template

--- app/models/product_image.py
+++ app/models/product_image.py
@@ -3,6 +3,7 @@ from app import db
 
 class ProductImage(db.Model):
     __tablename__ = "product_images"
+    
     id = db.Column(db.Integer, primary_key=True)
     image_url = db.Column(db.String(255), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
