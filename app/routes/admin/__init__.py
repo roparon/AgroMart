907a8800bf09bdf2e16a92ae458bd9f6c7c606a1
Aron Rop <aaronrop40@gmail.com>
2026-08-04 21:26:54 +0300

    simple modification

--- app/routes/admin/__init__.py
+++ app/routes/admin/__init__.py
@@ -2,8 +2,6 @@ from flask import Blueprint
 
 
 admin_bp = Blueprint("admin", __name__,)
-
-
 from app.routes.admin.products import products_bp
 from app.routes.admin.categories import categories_bp
 from app.routes.admin.orders import orders_bp
