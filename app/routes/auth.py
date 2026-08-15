d2bee9081abff80dc603fa5d01a7b08fae7121ce
Aron Rop <aaronrop40@gmail.com>
2026-08-15 21:37:17 +0300

    reaarranging my codebase

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
-
 from app import db
 from app.forms import RegisterForm, LoginForm
 from app.models.user import User
